7ce86a6f96c688caa6678f038e51514fdfbf186d
cmitchell7376 <cmitchell7376@yahoo.com>
2019-06-27 22:21:25 -0500

    initial commit

---
+++ src/main/java/com/example/myInventory/MyInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.myInventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyInventoryApplication.class, args);
+	}
+
+}
